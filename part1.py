354208be7ed6e1648e98e4412a426d260f2c1669
Matheus Tagliatti <163213259+Taglii@users.noreply.github.com>
2025-09-16 17:27:35 -0300

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,21 @@
+def Falar_nome(nome):
+    print(f"Olá {nome}")
+    
+def Soma_numero(n1, n2):
+    soma = n1 + n2
+    print(f"A soma de {n1} + {n2} é {soma}")
+
+def Media_numeros(numero1,numero2,numero3):
+    soma = numero1 + numero2 + numero3
+    media = soma / 3
+    print(f"A media dos numeros: {numero1}, {numero2} e {numero3} é: {media:.2f}")
+    
+def Medir_raio(raio):
+    area = 3.14159 * (raio ** 2 )
+    print (f"A aréa do raio de {raio} é {area}")
+    
+    
+
+
+
+    
